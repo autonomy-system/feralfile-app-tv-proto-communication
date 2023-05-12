edd550688c581111a371dff8bd1b30341a3b3a81
phuoc <phuoc@bitmark.com>
2023-05-12 17:07:26 +0700

    export
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/autonomy_tv_proto.dart
+++ lib/autonomy_tv_proto.dart
@@ -4,3 +4,4 @@ export 'src/generated/canvas_control/canvas_control.pb.dart';
 export 'src/generated/canvas_control/canvas_control.pbenum.dart';
 export 'src/generated/canvas_control/canvas_control.pbgrpc.dart';
 export 'src/generated/canvas_control/canvas_control.pbjson.dart';
+export 'package:grpc/grpc.dart';
