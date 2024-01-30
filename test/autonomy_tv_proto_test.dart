6d1cbbe1ec1a6c5919267de9eab3d3926bc83654
PuPha <nguyensanghso@gmail.com>
2024-01-30 18:22:26 +0700

    Rename Package

--- test/autonomy_tv_proto_test.dart
+++ test/autonomy_tv_proto_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:autonomy_tv_proto/autonomy_tv_proto.dart';
-
 void main() {
   test('adds one to input values', () {
     final calculator = Calculator();
