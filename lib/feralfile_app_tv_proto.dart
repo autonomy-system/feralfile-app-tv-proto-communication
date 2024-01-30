e050458005e8eea9ef8988bdd1c3a1469a9572ff
PuPha <nguyensanghso@gmail.com>
2024-01-30 18:22:26 +0700

    Rename Package

--- lib/feralfile_app_tv_proto.dart
+++ lib/feralfile_app_tv_proto.dart
@@ -1,8 +1,9 @@
-library autonomy_tv_proto;
+library feralfile_app_tv_proto;
 
+export 'package:grpc/grpc.dart';
+
+export 'models/canvas_device.dart';
 export 'src/generated/canvas_control/canvas_control.pb.dart';
 export 'src/generated/canvas_control/canvas_control.pbenum.dart';
 export 'src/generated/canvas_control/canvas_control.pbgrpc.dart';
 export 'src/generated/canvas_control/canvas_control.pbjson.dart';
-export 'package:grpc/grpc.dart';
-export 'models/canvas_device.dart';
