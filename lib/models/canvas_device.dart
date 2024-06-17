93003081ad1dcdf5131b0955cf7f047365774305
PuPha <nguyensanghso@gmail.com>
2024-06-17 14:11:36 +0700

    add platform

--- lib/models/canvas_device.dart
+++ lib/models/canvas_device.dart
@@ -1,8 +1,11 @@
+import 'package:feralfile_app_tv_proto/models/model.dart';
+
 class CanvasDevice {
   final String deviceId; //hardware id
   final String locationId; // location id
   final String topicId; // topic id
   final String name; // device name
+  final DevicePlatform platform; // device platform
 
   // constructor
   CanvasDevice({
@@ -10,6 +13,7 @@ class CanvasDevice {
     required this.locationId,
     required this.topicId,
     required this.name,
+    required this.platform,
   });
 
   //fromJson method
@@ -18,6 +22,7 @@ class CanvasDevice {
         locationId: json["locationId"] as String,
         topicId: json["topicId"] as String,
         name: json["name"] as String,
+        platform: DevicePlatform.values[json["platform"] as int],
       );
 
   // toJson
@@ -26,6 +31,7 @@ class CanvasDevice {
         "locationId": locationId,
         "topicId": topicId,
         "name": name,
+        "platform": platform.index,
       };
 
   // copyWith
@@ -34,12 +40,14 @@ class CanvasDevice {
     String? locationId,
     String? topicId,
     String? name,
+    DevicePlatform? platform,
   }) {
     return CanvasDevice(
       deviceId: deviceId ?? this.deviceId,
       locationId: locationId ?? this.locationId,
       topicId: topicId ?? this.topicId,
       name: name ?? this.name,
+      platform: platform ?? this.platform,
     );
   }
 
